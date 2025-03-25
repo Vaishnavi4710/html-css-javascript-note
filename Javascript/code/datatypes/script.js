57abf60954f3ed7560bb0c06b4799d004504f6c4
Vaishnavi4706 <vaishnaviyerumalli@gmail.com>
2025-03-26 00:18:19 +0530

    updated

---
+++ Javascript/code/datatypes/script.js
@@ -0,0 +1,25 @@
+let a=5;
+console.log(typeof a) //number
+
+a="vaishnavi"
+console.log(typeof a) //string
+
+a=1234
+console.log(typeof a)//number
+
+a=true;
+console.log(typeof a) //boolean
+
+
+
+let v = 5;
+v=5 +'5'
+v="vaishu" + 8;
+console.log(v,typeof v);
+
+
+h = true;
+h=1 +  true;
+console.log(h,typeof h);
+
+
--- Javascript/code/first/script.js
+++ Javascript/code/first/script.js
@@ -1,1 +1,43 @@
+//This is SIngle line comment
+/* it is a multi
+line
+cooment*/
 console.log("bye world")
+alert("Happy holi")
+let x=confirm("are you sure to delete")
+console.log(x)
+
+/* var*/
+/*function x ()
+{
+    if (true)
+
+    {
+        console.log(a); //we can acess values before initilization using var
+        var a=5; //var scope is function
+        var a=10; //redeclare is valid using var
+        a=25; //reassing is also valid using var
+
+    }
+    console.log(a)
+}
+x();
+
+
+
+
+function x ()
+{
+    if (true)
+
+    {
+        //console.log(a); //we can acess values before initilization using var
+        let a=5 //let ,const scope is block
+        //let a=10  //redeclare is not valid using var
+       a=25; //reassing is also valid using var
+        console.log(a)
+
+    }
+    
+}
+x()*/
